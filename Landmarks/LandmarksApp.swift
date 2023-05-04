ae0743282c63673a38b45a5f5068fd39599fe509
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-04 18:49:04 -0300

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Guilherme Victor on 04/05/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
