5cb61d66603dbe9209338ed7450b8e155fe10c05
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-04 20:59:16 -0300

    feat: adding dynamic list and loading data from a json

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Guilherme Victor on 04/05/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
