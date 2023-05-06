b5997a50f373f47a5ff560c0f73eca6fc2783d00
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2023-05-05 21:04:47 -0300

    feat: adding a favorite filter and button to add favorite landmark

--- Landmarks/Views/Landmarks/LandmarkDetail.swift
+++ Landmarks/Views/Landmarks/LandmarkDetail.swift
@@ -8,7 +8,12 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
     var landmark: Landmark
+    
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
     
     var body: some View {
         ScrollView {
@@ -16,7 +21,11 @@ struct LandmarkDetail: View {
                 MapView(coordinate: landmark.locationCoordinate).frame(height: 300).ignoresSafeArea(edges: .top)
                 CircleImage(image: landmark.image).offset(y: -130).padding(.bottom, -130)
                 VStack(alignment: .leading) {
-                    Text(landmark.name).font(.title)
+                    HStack {
+                        Text(landmark.name).font(.title).font(.title)
+                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    }
+                   
                     HStack {
                         Text(landmark.park)
                         Spacer()
@@ -35,7 +44,8 @@ struct LandmarkDetail: View {
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
+    static let modelData = ModelData()
     static var previews: some View {
-        LandmarkDetail(landmark: landmarks[0])
+        LandmarkDetail(landmark: ModelData().landmarks[0]).environmentObject(modelData)
     }
 }
---
+++ Landmarks/Views/Landmarks/LandmarkRow.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Guilherme Victor on 05/05/23.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkRow_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkRow()
+    }
+}
